5c0fd954649953fab54b50546c9627c852d8d59f
Andreas Prucha (Abexto/Helicon) <andreas.prucha@gmail.com>
2018-04-18 17:32:39 +0200

    Init

---
+++ src/Doctrine.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright 2018 Andreas Prucha, Abexto - Helicon Software Development.
+ */
+
+namespace abexto\amylian\yii\doctrine\dbal;
+
+/**
+ * Description of Doctrine
+ *
+ * @author Andreas Prucha, Abexto - Helicon Software Development
+ */
+class Doctrine extends \abexto\amylian\yii2\doctrine\base\Doctrine
+{
+    //put your code here
+}
