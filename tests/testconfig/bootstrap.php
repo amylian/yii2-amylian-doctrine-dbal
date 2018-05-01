a84344edff236fc4be25a0f896ee74beb96965d9
Andreas Prucha (Abexto/Helicon) <andreas.prucha@gmail.com>
2018-05-01 12:17:22 +0200

    Preparing for travis

--- tests/testconfig/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define ('ABEXTO_AMYLIAN_VENDOR_PATH' , __DIR__.'/../../../..');
-require_once ABEXTO_AMYLIAN_VENDOR_PATH.'/autoload.php';
-abexto\amylian\yii\phpunit\Bootstrap::initEnv(__FILE__);
-
